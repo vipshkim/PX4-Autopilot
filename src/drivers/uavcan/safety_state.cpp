178a3134a91861f34ef33521dcabe9cad56dbd31
vipshkim <vipsh.kim@gmail.com>
2026-01-05 21:17:18 +0900

    Update safety state logic in periodic_update
    Commented out actuator armed subscription code and set safety state to 'OFF'.

--- src/drivers/uavcan/safety_state.cpp
+++ src/drivers/uavcan/safety_state.cpp
@@ -61,6 +61,7 @@ int UavcanSafetyState::init()
 
 void UavcanSafetyState::periodic_update(const uavcan::TimerEvent &)
 {
+	/*
 	actuator_armed_s actuator_armed;
 
 	if (_actuator_armed_sub.update(&actuator_armed)) {
@@ -75,4 +76,8 @@ void UavcanSafetyState::periodic_update(const uavcan::TimerEvent &)
 
 		(void)_safety_state_pub.broadcast(cmd);
 	}
+	*/
+	ardupilot::indication::SafetyState cmd;
+        cmd.status = cmd.STATUS_SAFETY_OFF; // 255
+	(void)_safety_state_pub.broadcast(cmd);
 }
